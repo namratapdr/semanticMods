85ae25d0740ccb1a72fd71918a0da54244ff0a42
namratapdr <namratapodder138@gmail.com>
2021-07-13 01:54:55 +0530

    Added mods-feature module

---
+++ src/app/mods-feature/mods-feature.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ModsFeatureRoutingModule } from './mods-feature-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ModsFeatureRoutingModule
+  ]
+})
+export class ModsFeatureModule { }
